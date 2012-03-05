8bf85d1b146f91c0c69233196d2152a35b19c52c
Dominik Sander <git@dsander.de>
2012-03-05 01:22:48 +0100

    documentation for the DataCenter class

--- lib/profitbricks/data_center.rb
+++ lib/profitbricks/data_center.rb
@@ -2,11 +2,15 @@ module Profitbricks
   class DataCenter < Profitbricks::Model
     has_many :servers
 
+    # Deletes an empty Virtual Data Center. All components must be removed first.
+    # @return [true] true success, false otherwise
     def delete
       response = Profitbricks.request :delete_data_center, "<dataCenterId>#{self.id}</dataCenterId>"
-      return true if response.to_hash[:delete_data_center_response][:return]
+      response.to_hash[:delete_data_center_response][:return] ? true : false
     end
 
+    # Removes all components from the current Virtual Data Center.
+    # @return The current Virtual Data Center
     def clear
       response = Profitbricks.request :clear_data_center, "<dataCenterId>#{self.id}</dataCenterId>"
       @provisioning_state = nil
@@ -14,7 +18,11 @@ module Profitbricks
       @storages = []
       return self if response.to_hash[:clear_data_center_response][:return]
     end
-
+    
+    # Renames the Virtual Data Center. 
+    #
+    # @param [String] Name
+    # @return [DataCenter] The renamed DataCenter
     def rename(name)
       response = Profitbricks.request :update_data_center,
                   "<arg0><dataCenterId>#{self.id}</dataCenterId><dataCenterName>#{name}</dataCenterName></arg0>"
@@ -23,17 +31,21 @@ module Profitbricks
       end
       self
     end
+    alias_method :name=, :rename
 
-    def name=(name)
-      self.rename(name)
-    end
-
+    # This is a lightweight function for pooling the current provisioning state of the Virtual Data 
+    # Center. It is recommended to use this function for large Virtual Data Centers to query request 
+    # results.
+    #
+    # @return [String] Provisioning State of the target Virtual Data Center (INACTIVE, INPROCESS, AVAILABLE, DELETED)
     def update_state
       response = Profitbricks.request :get_data_center_state, "<dataCenterId>#{self.id}</dataCenterId>"
       @provisioning_state = response.to_hash[:get_data_center_state_response][:return]
       self.provisioning_state
     end
 
+    # Creates a Server in the current Virtual Data Center, automatically sets the :data_center_id
+    # @see Profitbricks::Server#create
     def create_server(options)
       s = Server.create(options.merge(:data_center_id => self.id))
       @servers << s
@@ -41,6 +53,9 @@ module Profitbricks
     end
 
     class << self
+      # Returns a list of all Virtual Data Centers created by the user, including ID, name and version number.
+      # 
+      # @return [Array <DataCenter>] Array of all available DataCenter
       def all
         resp = Profitbricks.request :get_all_data_centers
         datacenters = resp.to_hash[:get_all_data_centers_response][:return]
@@ -48,12 +63,20 @@ module Profitbricks
           PB::DataCenter.new(dc)
         end
       end
-
+      
+      # Creates and saves a new, empty Virtual Data Center.
+      #
+      # @param [String] Name of the Virtual Data Center (can not start with or contain (@, /, \\, |, ", '))
+      # @return [DataCenter] The newly created Virtual Data Center
       def create(name)
         response = Profitbricks.request :create_data_center, "<dataCenterName>#{name}</dataCenterName>"
         PB::DataCenter.new(response.to_hash[:create_data_center_response][:return].merge(:data_center_name => name))
       end
 
+      # Finds a Virtual Data Center
+      # @param [Hash] options either name or id of the Virtual Data Center
+      # @option options [String] :name The name of the Virtual Data Center
+      # @option options [String] :id The id of the Virtual Data Center
       def find(options = {})
         if options[:name]
           dc = PB::DataCenter.all().select { |d| d.name == options[:name] }.first
